e96748f06f6eb26c1747eff07a47de8c4d43fe5f
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-27 14:43:27 -0600

    added dependencies and set up api route

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  USER: 'burnerbb123@gmail.com',
+  PASS: 'RoyalMac21',
+
+  TO: 'thebuffcoder@gmail.com',
+  FROM: 'burnerbb123@gmail.com'
+}
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const nodemailer = require('nodemailer');
+const creds = require('./config/config.js');
+const app = express();
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.post('/api/form', (req, res) => {
+  // console.log(req.body)
+
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+    <h3>Contact Details<h3>
+    <ul style="list-style-type:none">
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.number}</li>
+      <li>Package: ${req.body.select}</li>
+      <li>Date of Event: ${req.body.date}</li>
+    </ul>
+    <h3>Message:</h3>
+    <p>${req.body.message}</p>
+      `
+
+
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: creds.USER,
+        pass: creds.PASS
+      },
+      // tls: {
+      //   rejectUnauthorized: false
+      // }
+    });
+
+    let mailOptions = {
+      from: creds.FROM,
+      to: creds.TO,
+      subject: 'New Message',
+      text: req.body.message,
+      html: htmlEmail
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return console.log(err)
+      }
+
+      console.log('Message sent: %s', info.message)
+      console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+    })
+
+  })
+})
+
+const PORT = process.env.PORT || 3001
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
