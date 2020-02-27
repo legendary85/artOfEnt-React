ef9d451ddc12f44bf7e84015a1aa2adc425c6b31
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-27 15:08:00 -0600

    added all images from earlier projectt

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavigationBar from './components/layout/navbar/NavigationBar.js'
+import Landing from './components/landing/Landing.js'
+import About from './components/about/About.js'
+import Packages from './components/packages/Packages.js'
+import Submitted from './components/submitted/Submitted.js'
+// // import DemoContactForm from './components/contact/DemoContactForm.js'
+import Contact from './components/contact/Contact.js'
+import Footer from './components/layout/footer/Footer.js'
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+      <NavigationBar />
+      <section>
+        <Switch>
+          <Route exact path='/' component={Landing}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/packages' component={Packages}></Route>
+          <Route exact path='/contact' component={Contact}></Route>
+          <Route exact path='/submitted' component={Submitted}></Route>
+          <Route path='/' render={() => <div style={{ color: 'white', display: 'grid', justifyItems: 'center', height: '70vh', alignItems: 'center' }}>404</div>}></Route>
+          {/* <Route exact path='/contact' component={ContactForm}></Route> */}
+        </Switch>
+      </section>
+      <Footer style={{ color: 'blue' }} />
+    </Router>
   );
 }
 
