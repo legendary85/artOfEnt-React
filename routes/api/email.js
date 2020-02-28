c7b546b1352c2680e7eb8590ab26ec403b4ccf47
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-27 18:11:22 -0600

    added validation to api route

--- routes/api/email.js
+++ routes/api/email.js
@@ -28,18 +28,19 @@ router.post('/', [
     .isEmpty(),
   check('message', 'Please submit a message with a minimum of 5 characters.').not()
     .isEmpty().isLength({ min: 5 }),
-], (req, res) => {
-  // Object of data being sent to route
-  console.log(req.body)
+],
+  (req, res) => {
+    // Object of data being sent to route
+    console.log(req.body)
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-  res.send('User route')
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    res.send('User route')
 
-  nodemailer.createTestAccount((err, account) => {
-    const htmlEmail = `
+    nodemailer.createTestAccount((err, account) => {
+      const htmlEmail = `
     <h3>Contact Details<h3>
     <ul style="list-style-type:none">
       <li>Name: ${req.body.name}</li>
@@ -52,39 +53,39 @@ router.post('/', [
     <p>${req.body.message}</p>
       `
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        // user: connectEmail.USER,
-        // pass: connectEmail.PASS
-        user: creds.USER,
-        pass: creds.PASS
-      },
-      // tls: {
-      //   rejectUnauthorized: false
-      // }
-    });
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          // user: connectEmail.USER,
+          // pass: connectEmail.PASS
+          user: creds.USER,
+          pass: creds.PASS
+        },
+        // tls: {
+        //   rejectUnauthorized: false
+        // }
+      });
 
-    let mailOptions = {
-      // from: connect.FROM,
-      // to: connectEmail.TO,
-      from: creds.FROM,
-      to: creds.TO,
-      subject: 'New Message',
-      text: req.body.message,
-      html: htmlEmail
-    }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        return console.log(err)
+      let mailOptions = {
+        // from: connect.FROM,
+        // to: connectEmail.TO,
+        from: creds.FROM,
+        to: creds.TO,
+        subject: 'New Message',
+        text: req.body.message,
+        html: htmlEmail
       }
 
-      console.log('Message sent: %s', info.message)
-      console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
-    })
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          return console.log(err)
+        }
 
+        console.log('Message sent: %s', info.message)
+        console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+      })
+
+    })
   })
-})
 
 module.exports = router;
