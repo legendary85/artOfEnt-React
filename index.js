db247f03476b7aaf02d28525331a8edf4e83db8f
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-27 22:53:54 -0600

    set up static assets and folder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+// const bodyparser = require('body-parser');
 const app = express();
+const path = require('path');
 
 
 // Init Middleware with express we no longer need bodyparser
@@ -13,7 +14,15 @@ app.use(express.json({ extended: false }))
 app.use('/api/form', require('./routes/api/email'))
 app.use('/api/email', require('./routes/api/email'))
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001
 
