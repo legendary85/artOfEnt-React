ecde078316c6feb848581c62eeb24a528a12e8df
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-27 20:07:53 -0600

    setting up redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,28 +9,33 @@ import Submitted from './components/submitted/Submitted.js'
 import Contact from './components/contact/Contact.js'
 import Footer from './components/layout/footer/Footer.js'
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 import './App.css';
 
 const App = () => (
-
-  <Router>
-    <Fragment>
-      <NavigationBar />
-      <Route exact path='/' component={Landing}></Route>
-      <section className='container'>
-        <Switch>
-          <Route exact path='/about' component={About}></Route>
-          <Route exact path='/packages' component={Packages}></Route>
-          <Route exact path='/contact' component={Contact}></Route>
-          <Route exact path='/submitted' component={Submitted}></Route>
-          {/* <Route path='/' render={() => <div style={{ color: 'white', display: 'grid', justifyItems: 'center', height: '70vh', alignItems: 'center' }}>404</div>}></Route> */}
-          {/* <Route exact path='/contact' component={ContactForm}></Route> */}
-        </Switch>
-      </section>
-      <Footer />
-    </Fragment>
-  </Router>
+  <Provider>
+    <Router>
+      <Fragment>
+        <NavigationBar />
+        <Route exact path='/' component={Landing}></Route>
+        <section className='container'>
+          <Switch>
+            <Route exact path='/about' component={About}></Route>
+            <Route exact path='/packages' component={Packages}></Route>
+            <Route exact path='/contact' component={Contact}></Route>
+            <Route exact path='/submitted' component={Submitted}></Route>
+            {/* <Route path='/' render={() => <div style={{ color: 'white', display: 'grid', justifyItems: 'center', height: '70vh', alignItems: 'center' }}>404</div>}></Route> */}
+            {/* <Route exact path='/contact' component={ContactForm}></Route> */}
+          </Switch>
+        </section>
+        <Footer />
+      </Fragment>
+    </Router>
+  </Provider>
 )
 
 export default App;
